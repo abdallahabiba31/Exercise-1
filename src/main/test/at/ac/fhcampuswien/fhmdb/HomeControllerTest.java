2e92d35264f85dc33b2c6c06f118577a1658a65e
Ami5412 <ahmed.dawoud@stud.fh-campuswien.ac.at>
2023-03-12 21:32:37 +0100

    Exercice 1

--- src/main/test/at/ac/fhcampuswien/fhmdb/HomeControllerTest.java
+++ src/main/test/at/ac/fhcampuswien/fhmdb/HomeControllerTest.java
@@ -16,6 +16,58 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class HomeControllerTest {
     HomeController controller = new HomeController();
+
+    @Test
+    void movie_list_is_sorted_ascending_if_button_displays_desc(){
+        // GIVEN
+        HomeController controller = new HomeController();
+        //create list that is going to be tested:
+        ObservableList<Movie> exampleMovies = FXCollections.observableArrayList();
+        Movie movie1 = new Movie("a-title1", "description1", List.of(Genre.ANIMATION));
+        Movie movie2 = new Movie("c-title2", "description2", List.of(Genre.ROMANCE));
+        Movie movie3 = new Movie("b-title3", "description3", List.of(Genre.ROMANCE));
+        exampleMovies.add(movie1);
+        exampleMovies.add(movie2);
+        exampleMovies.add(movie3);
+
+        // WHEN
+        ObservableList<Movie> actual = controller.sortMovies(exampleMovies, "Sort (asc)"); //actual
+
+        // THEN
+        //create expected list:
+        ObservableList<Movie> expected = FXCollections.observableArrayList();
+        expected.add(movie1);
+        expected.add(movie3);
+        expected.add(movie2);
+        //test
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void movie_list_is_sorted_descending_if_button_displays_asc(){
+        // GIVEN
+        HomeController controller = new HomeController();
+        //create list that is going to be tested:
+        ObservableList<Movie> exampleMovies = FXCollections.observableArrayList();
+        Movie movie1 = new Movie("a-title1", "description1", List.of(Genre.ANIMATION));
+        Movie movie2 = new Movie("c-title2", "description2", List.of(Genre.ROMANCE));
+        Movie movie3 = new Movie("b-title3", "description3", List.of(Genre.ROMANCE));
+        exampleMovies.add(movie1);
+        exampleMovies.add(movie2);
+        exampleMovies.add(movie3);
+
+        // WHEN
+        ObservableList<Movie> actual = controller.sortMovies(exampleMovies, "Sort (desc)"); //actual
+
+        // THEN
+        //create expected list:
+        ObservableList<Movie> expected = FXCollections.observableArrayList();
+        expected.add(movie2);
+        expected.add(movie3);
+        expected.add(movie1);
+        //test
+        assertEquals(expected, actual);
+    }
     /*@Test
     void display_only_movies_with_selected_genre(){
 
