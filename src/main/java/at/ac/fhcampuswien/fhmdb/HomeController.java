0467817c80d3a0aea0777f66c79c2ec11bf6a615
abdallahabiba31 <abdallahabiba97@gmail.com>
2023-03-12 20:18:31 +0100

    - updatet everything / new version

--- src/main/java/at/ac/fhcampuswien/fhmdb/HomeController.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/HomeController.java
@@ -36,10 +36,14 @@ public class HomeController implements Initializable {
 
     @FXML
     public JFXButton sortBtn;
+    @FXML
+    public JFXButton clearBtn;
 
     public List<Movie> allMovies = Movie.initializeMovies();
 
     private final ObservableList<Movie> observableMovies = FXCollections.observableArrayList();   // automatically updates corresponding UI elements when underlying data changes
+    private final ObservableList<Movie> moviesByGenre = FXCollections.observableArrayList();
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -59,46 +63,141 @@ public class HomeController implements Initializable {
         // either set event handlers in the fxml file (onAction) or add them here
 
         // Sort button example:
-        /*sortBtn.setOnAction(actionEvent -> {
-            if(sortBtn.getText().equals("Sort (asc)")) {
-                // TODO sort observableMovies ascending
-                sortBtn.setText("Sort (desc)");
-                observableMovies.sort(Comparator.comparing(Movie::getTitle));
-                //sortState = SortState.ASCENDING;
-
-            } else {
-                // TODO sort observableMovies descending
-                sortBtn.setText("Sort (asc)");
-            }
+        sortBtn.setOnAction(actionEvent -> {
+            sortMovies(observableMovies, sortBtn.getText());
         });
-        searchBtn.setOnAction(actionEvent -> {
-            Genres selectedGenre = (Genres) genreComboBox.getValue();
-
-
-            if (genreComboBox.getValue().equals(selectedGenre)) {
-                observableMovies.removeIf(movie -> !movie.getGenre().contains(selectedGenre));
-            }
+        /*searchBtn.setOnAction(actionEvent -> {
+            filterMoviesGenre(allMovies, (Genres) genreComboBox.getValue());
+            filterMoviesSearch(moviesByGenre, searchField.getText().toLowerCase());
+            //sortMovies(moviesByGenre, sortBtn.getText());
 
         });*/
+        clearBtn.setOnAction(actionEvent -> {
+            clearFilter();
+        });
+        //SEARCH BUTTON - ACTION EVENT
+        searchBtn.setOnAction(actionEvent -> {
+            genreFilter(allMovies, genreComboBox.getValue() != null ? genreComboBox.getValue().toString() : "NO_FILTER");
+            searchQuery(moviesByGenre, searchField.getText().toLowerCase());
+            sortMovies(moviesByGenre,sortBtn.getText());
+        });
 
     }
-
-    public void sortMovies(ActionEvent actionEvent) {
-        if (sortBtn.getText().equals("Sort (asc)")) {
+    public void sortMovies(ObservableList<Movie> movies, String sortBtnText) {
+        if (sortBtnText.equals("Sort (asc)")) {
             // TODO sort observableMovies ascending
             sortBtn.setText("Sort (desc)");
-            observableMovies.sort(Comparator.comparing(Movie::getTitle));
+            movies.sort(Comparator.comparing(Movie::getTitle));
 
         } else {
             // TODO sort observableMovies descending
             sortBtn.setText("Sort (desc)");
-            observableMovies.sort(Comparator.comparing(Movie::getTitle).reversed());
+            movies.sort(Comparator.comparing(Movie::getTitle).reversed());
             sortBtn.setText("Sort (asc)");
 
         }
     }
+    public void clearFilter() {
+        //clear the combobox and then set the text of it
+        genreComboBox.getSelectionModel().clearSelection();
+        genreComboBox.setPromptText("Filter by Genre");
 
-    public void filterMovies(ActionEvent actionEvent) {
+        //showing all the movies
+        //observableMovies.addAll(allMovies);
+        observableMovies.clear();
+        observableMovies.setAll(allMovies);
+        searchField.clear();
+    }
+    public List<Movie> genreFilter(List<Movie> allMovies, String genreSelection) {
+        //clear list before adding the movies or else same movie will be added multiple times
+        moviesByGenre.clear();
+
+        if (!genreSelection.equals("NO_FILTER")) {
+            for (Movie movies : allMovies) {
+                List<Genres> genres = movies.getGenre();
+
+                if (genres.toString().contains(genreSelection)) {
+                    moviesByGenre.add(movies);
+                }
+            }
+            observableMovies.setAll(moviesByGenre);
+        } else {
+            moviesByGenre.setAll(allMovies);// if "no filter" is selected, show all movies, moviesByGenre will be passed on to filterQuery
+        }
+        return moviesByGenre;
+    }
+
+
+    public List<Movie> searchQuery(List<Movie> moviesByGenre, String searchQuery) {
+        ObservableList<Movie> movieList = FXCollections.observableArrayList();
+
+        movieList.clear();
+
+        if(!searchQuery.contains(" ")) {      //!searchQuery.isEmpty() replaced --> search field is never empty because of white space
+            for (Movie movieLoop : moviesByGenre) {     // loops the already filtered movie list (by genre) and adds the movies it finds there to the new movieList
+                String title = movieLoop.getTitle().toLowerCase();
+                String description = movieLoop.getDescription().toLowerCase();
+
+                // Check if the title or description of the movie  contains the search query
+                if (title.contains(searchQuery) || description.contains(searchQuery)) {
+                    movieList.add(movieLoop);
+                }
+                // Set the observableMovies list to the filtered movieList
+                observableMovies.setAll(movieList);
+            }
+        }
+        return movieList;
+    }
+
+    /*public List<Movie> filterMoviesGenre(List<Movie> allMovies, Genres genreSelection) {
+
+        //System.out.println(genreSelection);
+        //System.out.println(allMovies);
+        List<Movie> rem = allMovies.stream()
+                .filter(q -> q.getGenre().contains(genreSelection))
+                .collect(Collectors.toList());
+        //System.out.println(rem);
+
+        //removing the not filtered elements
+        //reverse system somehow
+        allMovies.removeIf(q -> !rem.contains(q));
+        //System.out.println(allMovies);
+
+        return allMovies;
+    }
+
+    public List<Movie> filterMoviesSearch(List<Movie> allMovies, String searchQuery) {
+
+        List<Movie> filteredMovies = new ArrayList<>();
+        String search = searchQuery.toLowerCase();
+
+        System.out.println(allMovies);
+        if (search != null) {
+            System.out.println(searchField.getText());
+
+            //check if search query not contains in the movie(title and description) and then remove this movie from the list
+            //allMovies.removeIf(x -> !x.getTitle().contains(search) && !x.getDescription().contains(search));
+            for (Movie movieLoop : allMovies) {     // loops the already filtered movie list (by genre) and adds the movies it finds there to the new movieList
+                String title = movieLoop.getTitle().toLowerCase();
+                String description = movieLoop.getDescription().toLowerCase();
+
+                // Check if the title or description of the movie  contains the search query
+                if (title.contains(searchQuery) || description.contains(searchQuery)) {
+                    filteredMovies.add(movieLoop);
+                }
+                // Set the observableMovies list to the filtered movieList
+                observableMovies.setAll(filteredMovies);
+            }
+            System.out.println(filteredMovies);
+            //return allMovies;
+        }
+        return observableMovies;
+
+    }*/
+
+
+
+    /*public void filterMovies(ActionEvent actionEvent) {
 
         String search = searchField.getText().toLowerCase();
         List<Movie> filteredMovies = new ArrayList<>();
@@ -137,6 +236,7 @@ public class HomeController implements Initializable {
         genreComboBox.setPromptText("Filter by Genre");
         //showing all the movies
         observableMovies.addAll(allMovies);
+    }*/
 
-}
+
 }
